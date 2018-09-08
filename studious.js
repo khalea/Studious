00f181d4b1ed4f1c5dc94f8cb20953a27d1a97e9
Khalea <khaleaberry@gmail.com>
2018-09-08 13:34:57 -0400

    Change innerhtml of info div to the form -- first stage

--- studious.js
+++ studious.js
@@ -3,4 +3,13 @@ function showForm() {
     while (myNode.firstChild) {
         myNode.removeChild(myNode.firstChild);
     }
+
+    let formCode = "<div class=\"card\" id=\"form\">\n" +
+        "        <div class=\"container\">\n" +
+        "            <h2><b class=\"cardTitle\">Sarah Tonin + Introductory Neuroscience</b></h2>\n" +
+        "            <p><i>Wanna study at Wells Quad?</i></p>\n" +
+        "        </div>\n" +
+        "    </div>"
+
+    document.getElementById('info').innerHTML = formCode;
 }
