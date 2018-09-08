641b411a6a05e4ecde07eb9b8a93926e1b359a31
Khalea <khaleaberry@gmail.com>
2018-09-08 17:15:45 -0400

    Changed form styles

--- studious.js
+++ studious.js
@@ -11,7 +11,7 @@ function showForm() {
         "                   <h1><b class=\"cardTitle\" style='font-size: 36px;'>Create Your Sesh.</b></h1>\n" + "<br>\n" +
         "                   <input type='text' class='formData' id='subject' placeholder='Subject' style='padding: 16px; margin: 8px'>\n" + "<br>\n" +
         "                   <input type='text' class='formData' id='purpose' placeholder='Purpose, Assignment, etc' style='padding: 16px; margin: 8px'>\n" + "<br>\n" +
-        "                   <input type='submit' value='Create your Sesh!'>\n" +
+        "                   <input type='submit' id='newSeshSubmit' value='Create'>\n" +
         "               </div>\n" +
         "               <script> requestIndex.addObject($('form').serializeArray()); </script>"
         "           </div>";
