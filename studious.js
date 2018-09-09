62ce13db69562c3a9cf763a4bbee5fae7b63aebb
Khalea <khaleaberry@gmail.com>
2018-09-08 21:41:15 -0400

    Updated form with additional Sesh params

--- studious.js
+++ studious.js
@@ -9,6 +9,8 @@ function showForm() {
         "              <script src=\"./algoliasearch.js\"></script>" +
         "               <div class=\"container\">\n" +
         "                   <h1><b class=\"cardTitle\" style='font-size: 36px;'>Create Your Sesh.</b></h1>\n" + "<br>\n" +
+        "                   <input type='text' class='formData' id='university' placeholder='University' style='padding: 16px; margin: 8px'> \n" + "<br>\n" +
+        "                   <input type='text' class='formData' id='building' placeholder='Building & Room #' style='padding: 16px; margin: 8px'> \n" + "<br>\n" +
         "                   <input type='text' class='formData' id='subject' placeholder='Subject' style='padding: 16px; margin: 8px'>\n" + "<br>\n" +
         "                   <input type='text' class='formData' id='purpose' placeholder='Purpose, Assignment, etc' style='padding: 16px; margin: 8px'>\n" + "<br>\n" +
         "                   <input type='submit' id='newSeshSubmit' value='Create'>\n" +
